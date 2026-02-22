931929189897c5ecf48bcdfe93d8e11f8e53b41e
Rishabh Srivastava <rishabhsriv@gmail.com>
2026-02-22 10:25:42 +0800

    better way of getting version instead of hard-coding it

--- defog/__init__.py
+++ defog/__init__.py
@@ -12,7 +12,9 @@ from typing import Optional, Union
 from defog.llm.llm_providers import LLMProvider
 import warnings
 
-from defog.__version__ import __version__
+from importlib.metadata import version
+
+__version__ = version("defog")
 
 SUPPORTED_DB_TYPES = [
     "postgres",
